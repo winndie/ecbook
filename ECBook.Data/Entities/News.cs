b22ac75c736a61ac29f4ee686125029cbfe1ce79
Winndie Fan <winndie@gmail.com>
2023-10-31 10:30:51 +0000

    Tech news

---
+++ ECBook.Data/Entities/News.cs
@@ -0,0 +1,9 @@
+﻿namespace ECBook.Data.Entities
+{
+    public class News
+    {
+        public string Uuid { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+    }
+}
--- ECBook/Pages/Index.cshtml.cs
+++ ECBook/Pages/Index.cshtml.cs
@@ -2,6 +2,7 @@
 using ECBook.Data;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
 
 namespace ECBook.App.Pages;
 public class IndexModel : PageModel
@@ -10,14 +11,32 @@ public class IndexModel : PageModel
 
     public List<Post> Posts { get; set; }
 
-    public IndexModel(Context context)
+    public readonly string NewsApi;
+
+    public News News { get; set; }
+
+    public IndexModel(Context context, IConfiguration Configuration)
     {
         DbContext = context;
+        string url = Configuration["NewsApiUrl"] + Configuration["NewsApiKey"];
+        NewsApi = url;
     }
 
     public async Task OnGetAsync()
     {
         await LoadPosts();
+        using (var httpClient = new HttpClient())
+        {
+            using (HttpResponseMessage response = await httpClient.GetAsync(NewsApi))
+            {
+                var data = JsonConvert.DeserializeObject<NewsResponse>(await response.Content.ReadAsStringAsync());
+
+                if (data != null && data.Data.Count() > 0)
+                {
+                    News = data.Data[0];
+                }
+            }
+        }
     }
 
     private async Task LoadPosts()
